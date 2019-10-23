017141c7d6d09faee193588578ce00ecfce18669
Krissy Lewis <kristianlewis03@gmail.com>
2019-10-22 21:29:12 -0700

    add comments for clarity

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -11,9 +11,10 @@ export default function useVisualMode(initial) {
         }
         setMode(nextMode);
     }
-    
+
     function back() {
         if(history.length > 1) {
+          //brings the sate back a step when there is at least 2 states in history
           const newHistory = history.splice(0, history.length - 1);
           setHistory(newHistory);
           setMode(newHistory[newHistory.length - 1]);
